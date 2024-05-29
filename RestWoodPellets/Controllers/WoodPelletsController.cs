a4fc2326dcbc175defadb4bac32d8617a62bfc53
Klaus Eigil Erik Avgo Thor Therkildsen <klau8393@edu.zealand.dk>
2024-05-29 23:11:24 +0200

    Add project files.

---
+++ RestWoodPellets/Controllers/WoodPelletsController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WoodPelletsLib;
+
+namespace RestWoodPellets.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WoodPelletsController : Controller
+    {
+        // Get: api/WoodPellets
+        [HttpGet]
+        public IEnumerable<WoodPellet> GetAll()
+        {
+            return WoodPelletManager.GetAll();
+        }
+        // Get: api/WoodPellets/5
+        [HttpGet("{id}", Name = "Get")]
+        public IActionResult GetById(int id)
+        {
+            var woodPellet = WoodPelletManager.GetById(id);
+            if (woodPellet == null)
+            {
+                return NotFound();// 404
+            }
+            return Ok(woodPellet);// 200
+        }
+        // Post: api/WoodPellets
+        [HttpPost]
+        public IActionResult Add(WoodPellet woodPellet)
+        {
+            WoodPelletManager.Add(woodPellet);
+            return CreatedAtRoute(nameof(GetById), new { id = woodPellet.Id }, woodPellet);
+        }
+        // Put: api/WoodPellets/5
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, WoodPellet woodPellet)
+        {
+            if (id != woodPellet.Id)
+            {
+                return BadRequest();// 400
+            }
+            try
+            {
+                WoodPelletManager.Update(woodPellet);
+            }
+            catch (System.Exception)
+            {
+                return NotFound();// 404
+            
+            }
+            return NoContent();// 204
+        }
+    }
+}
---
+++ RestWoodPellets/WoodPelletManager.cs
@@ -0,0 +1,48 @@
+﻿using WoodPelletsLib;
+
+namespace RestWoodPellets
+{
+    public class WoodPelletManager
+    {
+        private static List<WoodPellet> woodPellets = new List<WoodPellet>();
+        private static int nextId = 1;// Varia
+
+        static WoodPelletManager()
+        {
+            // Tilføg mindst 5 forskellige WoodPellets til listen
+            Add(new WoodPellet(nextId++, "BioWood", 4995, 4));
+            Add(new WoodPellet(nextId++, "BilligPille", 4125, 1));
+            Add(new WoodPellet(nextId++, "GoldenWoodPellet", 5995, 5));
+            Add(new WoodPellet(nextId++, "GoldWoodPellet", 5795, 5));
+            Add(new WoodPellet(nextId++, "EcoWood", 4595, 3));
+        }
+        public static IEnumerable<WoodPellet> GetAll()
+        {
+            return woodPellets;
+        }
+        public static WoodPellet GetById(int id)
+        {
+            return woodPellets.Find(wp => wp.Id == id);
+        }
+        public static void Add(WoodPellet woodPellet)
+        {
+            woodPellet.Id = nextId++;
+            //WoodPellet woodPellet = new WoodPellet(nextId++, brand, price, quality);
+            woodPellets.Add(woodPellet);
+        }
+        public static void Update(WoodPellet updatedWoodPellet)
+        {
+            WoodPellet existingWoodPellet = woodPellets.FirstOrDefault(wp => wp.Id == updatedWoodPellet.Id);
+            if (existingWoodPellet != null)
+            {
+                existingWoodPellet.Brand = updatedWoodPellet.Brand;
+                existingWoodPellet.Price = updatedWoodPellet.Price;
+                existingWoodPellet.Quality = updatedWoodPellet.Quality;
+            }
+            else
+            {
+                throw new ArgumentException($"WoodPellet with id {updatedWoodPellet.Id} not found.");
+            }
+        }
+    }
+}
